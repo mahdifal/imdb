289b87a4d71ec09425416d54071bb9c5f3b55fdf
Mahdi Falamarzi <mehdi.kindly@gmail.com>
2021-12-05 11:05:23 +0330

    App Offline alert

--- App.tsx
+++ App.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Screen from "./src/components/Screen";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { useNetInfo } from "@react-native-community/netinfo";
+import Offline from "./src/screens/Offline";
 
 export default function App() {
+  const netInfo = useNetInfo();
+
   return (
     <Screen>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
+      {!netInfo.isInternetReachable ? (
+        <Offline />
+      ) : (
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      )}
     </Screen>
   );
 }
